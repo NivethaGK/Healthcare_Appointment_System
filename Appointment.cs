3df0b47a4c1c0a603502a536063e6f5aaf919bf6
NivethaGK <110551369+NivethaGK@users.noreply.github.com>
2025-12-25 11:58:03 +0530

    Add files via upload

---
+++ Appointment.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HealthcareAppointmentSystem.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string PatientName { get; set; }
+        public string DoctorName { get; set; }
+        public DateTime AppointmentDate { get; set; }
+    }
+}
